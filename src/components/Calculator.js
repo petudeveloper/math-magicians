cafef13114a5294be02d70be5b0eae1d6ce35638
petumazo <davidalvarezmazzo@gmail.com>
2021-09-21 17:24:48 -0500

    Correct linters errors

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -1,21 +1,22 @@
-import React from "react";
+/* eslint-disable react/prefer-stateless-function */
+import React from 'react';
 
 export default class Calculator extends React.Component {
-    render() {
-        const digitsArray = [7, 8, 9, 4, 5, 6, 1, 2, 3, 0, '.'];
-        const utilsArray = ['AC', '+/-', '%'];
-        const operatorsArray = ['/', 'x', '-', '+', '='];
+  render() {
+    const digitsArray = [7, 8, 9, 4, 5, 6, 1, 2, 3, 0, '.'];
+    const utilsArray = ['AC', '+/-', '%'];
+    const operatorsArray = ['/', 'x', '-', '+', '='];
 
-        const utils = utilsArray.map((utilLabel, index) => <div id={`util-${index}`} key={`${utilLabel}`} className="util">{utilLabel}</div>);
-        const digits = digitsArray.map((digitLabel, index) => <div key={`${digitLabel}`} id={`digit-${index}`} className="digit">{digitLabel}</div>);
-        const operators = operatorsArray.map((operatorLabel, index) => <div id={`operator-${index}`} key={`${operatorLabel}`} className="operator">{operatorLabel}</div>);
-        return (
-            <div className="grid">
-                <div className="result">0</div>
-                {utils}
-                {digits}
-                {operators}
-            </div>
-        )
-    }
+    const utils = utilsArray.map((utilLabel, index) => <div id={`util-${index}`} key={`${utilLabel}`} className="util">{utilLabel}</div>);
+    const digits = digitsArray.map((digitLabel, index) => <div key={`${digitLabel}`} id={`digit-${index}`} className="digit">{digitLabel}</div>);
+    const operators = operatorsArray.map((operatorLabel, index) => <div id={`operator-${index}`} key={`${operatorLabel}`} className="operator">{operatorLabel}</div>);
+    return (
+      <div className="grid">
+        <div className="result">0</div>
+        {utils}
+        {digits}
+        {operators}
+      </div>
+    );
+  }
 }
