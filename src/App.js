3350a0c7127d7fd58aeff4b452cd1172c08d33c2
petumazo <davidalvarezmazzo@gmail.com>
2021-09-29 10:49:45 -0500

    Add basic NavBar to the app.

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
 import Calculator from './components/Calculator';
+import NavBar from './components/navBar';
 
 function App() {
   return (
     <div className="App">
+      <NavBar />
       <Calculator />
     </div>
   );
---
+++ src/components/navBar.js
@@ -0,0 +1,12 @@
+const NavBar = () => (
+  <nav>
+    <img className="mm-logo" src="./images/logo-MM.jpg" alt="math-magicians-logo" />
+    <ul>
+      <li>Home</li>
+      <li>Calculator</li>
+      <li>Quote</li>
+    </ul>
+  </nav>
+);
+
+export default NavBar;
